c67457a7fd54c4572f9721122e38b2ea008a165a
Prafful Kumar <kumarprafful.54@gmail.com>
2020-06-10 23:55:18 +0530

    django channels overview

---
+++ dchat/routing.py
@@ -0,0 +1,20 @@
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.conf.urls import url
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator, OriginValidator
+
+from chat.consumer import ChatConsumer
+
+
+application = ProtocolTypeRouter({
+    # Empty for now (http->django views is added by default)
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                [
+                    url(r"^messages/(?P<username>[\w.@+-]+)/", ChatConsumer),
+                ]
+            )
+        )
+    )
+})
